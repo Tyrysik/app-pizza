46abcfa60c87243119717408e6cb3b9342dd8299
Taras <tyrysik@ukr.net>
2020-10-05 12:17:23 +0300

    Create Git

---
+++ precache-manifest.dc2a8d803b36d37ca0c9298ef4bc5efd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee1508386a9d9491714640036f63272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc024d57a29d51e66a9",
+    "url": "/static/css/main.263449fa.chunk.css"
+  },
+  {
+    "revision": "ee29df3b085ebff61fac",
+    "url": "/static/js/2.666675bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.666675bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc024d57a29d51e66a9",
+    "url": "/static/js/main.89bfc033.chunk.js"
+  },
+  {
+    "revision": "705c69162250a325d3b6",
+    "url": "/static/js/runtime-main.f2e4f4fd.js"
+  },
+  {
+    "revision": "e8f60350c5e8058a2da4f3b78c513b6a",
+    "url": "/static/media/1.e8f60350.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
